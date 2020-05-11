7f868b812ab5e0439f8a8caa5ddd749d6ca075ab
Burdishka <oleksandr.burdeinyi@gmail.com>
2020-05-11 12:14:32 -0400

    8 kyu - What's wrong with these identifiers?

---
+++ 8kyu/Object.js
@@ -0,0 +1,13 @@
+/*
+8 kyu
+What's wrong with these identifiers?
+
+https://www.codewars.com/kata/56bb01de0e8b29de50000b19
+*/
+
+var Person = {
+    firsstName: "John",
+    secondName: "Doe",
+    emailAddress: "john.doe@email.com",
+    sex: "M"
+  }
