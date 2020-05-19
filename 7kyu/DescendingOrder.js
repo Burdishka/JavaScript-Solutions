e7e61170e813b6943a4b47df3a37963a0485b059
burdishka <oleksandr.burdeinyi@gmail.com>
2020-05-19 15:09:08 -0400

    7 kyu - Descending Order

---
+++ 7kyu/DescendingOrder.js
@@ -0,0 +1,10 @@
+/*
+7 kyu
+Descending Order
+
+https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+*/
+
+function descendingOrder(n){
+    return +String(n).split('').sort((a, b) => b - a).join('');
+}
