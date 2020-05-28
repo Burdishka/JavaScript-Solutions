88d06e8e858ef5f4992645521365e86787323251
burdishka <oleksandr.burdeinyi@gmail.com>
2020-05-28 10:34:39 -0400

    7 kyu - Make a function that does arithmetic!

---
+++ 7kyu/Arithmetic.js
@@ -0,0 +1,15 @@
+/*
+7 kyu
+Make a function that does arithmetic!
+
+https://www.codewars.com/kata/583f158ea20cfcbeb400000a
+*/
+
+function arithmetic(a, b, operator) {
+    return {
+        add: a + b,
+        subtract: a - b,
+        multiply: a * b,
+        divide: a / b
+    }[operator];
+}
